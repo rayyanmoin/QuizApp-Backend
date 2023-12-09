383dbe82e9ded553739aa1eb4d178ad90c769944
Rayyan Saqib <rayyansaqibmoin@gmail.com>
2023-12-09 15:24:23 +0500

    Empty commit to trigger push

---
+++ src/main/java/com/telusko/quizapp/service/QuestionService.java
@@ -0,0 +1,73 @@
+package com.telusko.quizapp.service;
+
+import com.telusko.quizapp.model.Question;
+import com.telusko.quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionDao questionDao;
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
+    }
+    public ResponseEntity<List<Question>> getQuestionbyCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        try{
+            questionDao.save(question);
+            return new ResponseEntity<>("success", HttpStatus.CREATED) ;
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+        return new ResponseEntity<>("oops, something went wrong", HttpStatus.BAD_REQUEST);
+
+    }
+
+    public ResponseEntity<String> deleteQuestion(Integer id) {
+        try {
+            if (questionDao.existsById(id)) {
+                questionDao.deleteById(id);
+                return new ResponseEntity<>("Deleted sucessfully", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("404 not found", HttpStatus.BAD_REQUEST);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Internal error accured", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByDifficultyLevel(String difficultylevel) {
+    try{
+        return new ResponseEntity<>(questionDao.findByDifficultyLevel(difficultylevel),HttpStatus.OK);
+
+    }
+    catch(Exception e){
+        e.printStackTrace();
+    }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+}
